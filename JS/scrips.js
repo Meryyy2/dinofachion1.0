c07c0ad8b6739d61e9c477039d688ed24cd9df1f
Maria Valverde <meryvaverde2@gmail.com>
2022-05-20 12:03:07 -0600

    gab

--- JS/scrips.js
+++ JS/scrips.js
@@ -1,38 +1,55 @@
-
 $(document).ready(function(){
-  $(window).scroll(function(){
-      if(this.scrollY > 20){
-          $('.navbarr').addClass("sticky");
-      }else{
-          $('.navbarr').removeClass("sticky");
-      }
-      if(this.scrollY > 500){
-          $('.scroll-up-btn').addClass("show");
-      }else{
-          $('.scroll-up-btn').removeClass("show");
-      }
-  });
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $('.navbarr').addClass("sticky");
+        }else{
+            $('.navbarr').removeClass("sticky");
+        }
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
 
-  /*scroll up btn*/
-  $('.scroll-up-btn').click(function(){
-      $('html').animate({scrollTop: 0});
-  });
+    /*scroll up btn*/
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+    });
 
-  /* Toggle menu */ 
-  $('.menu-btn').click(function(){
-      $('.navbarr .menu').toggleClass("active");
-      $('.menu-btn i').toggleClass("active");
-  });
+    /* Toggle menu */ 
+    $('.menu-btn').click(function(){
+        $('.navbarr .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
 
-  /* tryping word */
-  var typed = new Typed(".typing", {
-      strings: ["Desarrollador web", "Diseñador web", "Desarrollador aplicaciones"],
-      typeSpeed: 100,
-      backSpeed: 60,
-      loop: true 
-  });
+    /* tryping word */
+    var typed = new Typed(".typing", {
+        strings: ["", "Citas reservadas", "Diagnosticamos problemas de salud", "Vacunamos contra enfermedades", "Tratamos heridas", "Establecemos fracturas"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true 
+    });
 
-  
+    /* owl carousel */
+    $('.carsel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            }
+        }
+    });
 }); 
-
-
